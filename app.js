33fec7ea3810cb31d267b644a7d3cc7645c817b4
makzovisk <maksuelribeiro90@gmail.com>
2024-05-07 08:49:46 -0300

    Revert "Update app.js"
    This reverts commit 4ee94b548b685465195c87af70e1bca766269996.

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ function exibirTextoNaTela(tag, texto) {
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 5000');
-}
 
+}
 exibirMensagemInicial();
 
 function verificarChute() {
